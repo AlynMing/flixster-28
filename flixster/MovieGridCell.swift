d24c0e743c5536308617764a4e620bfe6c3ce3b5
Anthony-ferrari <anthonyferrari1611@gmail.com>
2020-09-28 13:34:53 -0700

    added tab bar controller, navigation view controller, moviegridcell to project.

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Anthony Malpartidaa on 9/28/20.
+//  Copyright © 2020 anthonyferrari. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
